2ea0f55d4552502aa2fbda3ccfa3a1b992c2b1de
Jay-Tech <Jayh160@yahoo.com>
2024-04-25 21:18:30 -0500

    update

--- GrblHAL_Sender/GrbLHAL Sender/States/RealTImeState.cs
+++ GrblHAL_Sender/GrbLHAL Sender/States/RealTImeState.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 
 public class RealTImeState
 {
+    public bool TLR { get; set; }
     public string RawRt { get; set; }
     public string[] MPos { get; set; } = [];
     public string[] Wco { get; set; } = [];
@@ -9,6 +12,11 @@ public class RealTImeState
     public bool MpgActive { get; set; }
     public bool Home { get; set; }
     public string SubState { get; set; }
+    public string WCS { get; set; }
+    public string Tool { get; set; }
+    public string ProgramedSpeed { get; set; }
+    public string ActualSpeed { get; set; }
+    public List<string> SignalStatus { get; set; } = [];
 
     public RealTImeState()
     {
