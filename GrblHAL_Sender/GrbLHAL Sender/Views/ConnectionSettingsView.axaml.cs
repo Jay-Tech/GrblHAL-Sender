222591ec8a5846675804962ef9d50ad824c3f984
Jay-Tech <Jayh160@yahoo.com>
2026-02-12 16:45:27 -0600

    added connection tool

---
+++ GrblHAL_Sender/GrbLHAL Sender/Views/ConnectionSettingsView.axaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using GrbLHAL_Sender.ViewModels;
+
+namespace GrbLHAL_Sender.Views;
+
+public partial class ConnectionSettingsView : UserControl
+{
+
+    public event Action? OnCloseRequested;
+    public ConnectionSettingsView()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(System.EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        UpdatePanelVisibility();
+
+        if (DataContext is ConnectionViewModel vm)
+        {
+            vm.PropertyChanged += (_, args) =>
+            {
+                if (args.PropertyName == nameof(ConnectionViewModel.SelectedConnectionType))
+                    UpdatePanelVisibility();
+            };
+        }
+    }
+
+    private void UpdatePanelVisibility()
+    {
+        if (DataContext is ConnectionViewModel vm)
+        {
+            SerialPanel.IsVisible = vm.SelectedConnectionType == 0;
+            TcpPanel.IsVisible = vm.SelectedConnectionType == 1;
+            WsPanel.IsVisible = vm.SelectedConnectionType == 2;
+        }
+    }
+
+    private void OnCloseClick(object? sender, RoutedEventArgs e)
+    {
+        OnCloseRequested?.Invoke();
+    }
+}
--- GrblHAL_Sender/GrbLHAL Sender/Views/MainView.axaml.cs
+++ GrblHAL_Sender/GrbLHAL Sender/Views/MainView.axaml.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Platform.Storage;
+using Avalonia.Threading;
 using GrbLHAL_Sender.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -11,22 +13,30 @@ namespace GrbLHAL_Sender.Views;
 public partial class MainView : UserControl
 {
     private MainViewModel _viewModel;
+    private DispatcherTimer? _longPressTimer;
+    private bool _longPressTriggered;
+    private Flyout? _connectionFlyout;
+    private ConnectionSettingsView? _connectionSettingsView;
+
     public MainView()
     {
         InitializeComponent();
 
-        //this.WhenActivated(d =>
-        //{
-        //    d(ViewModel.SelectFilesInteraction.RegisterHandler(this.InteractionHandler));
-        //});
+        // Get the Flyout and its content from the Connect button
+        _connectionFlyout = ConnectButton.Flyout as Flyout;
+        _connectionSettingsView = _connectionFlyout?.Content as ConnectionSettingsView;
 
+        // Set up long-press on Connect button
+        ConnectButton.AddHandler(PointerPressedEvent, ConnectButton_PointerPressed, handledEventsToo: true);
+        ConnectButton.AddHandler(PointerReleasedEvent, ConnectButton_PointerReleased, handledEventsToo: true);
+
+        // Wire up the ConnectRequested event from the ConnectionSettingsView
+        if (_connectionSettingsView != null)
+            _connectionSettingsView.OnCloseRequested += OnCloseRequested;
     }
-   
+
     IDisposable? _selectFilesInteractionDisposable;
 
-  
-    //Transform the model
-    
     protected override void OnDataContextChanged(EventArgs e)
     {
         _selectFilesInteractionDisposable?.Dispose();
@@ -39,11 +49,50 @@ public partial class MainView : UserControl
         }
         base.OnDataContextChanged(e);
     }
+
+    private void ConnectButton_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        _longPressTriggered = false;
+
+        _longPressTimer?.Stop();
+        _longPressTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromMilliseconds(500)
+        };
+        _longPressTimer.Tick += (_, _) =>
+        {
+            _longPressTimer.Stop();
+            _longPressTriggered = true;
+
+            // Show the connection settings flyout
+            _connectionFlyout?.ShowAt(ConnectButton);
+        };
+        _longPressTimer.Start();
+    }
+
+    private void ConnectButton_PointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _longPressTimer?.Stop();
+
+        if (_longPressTriggered)
+        {
+            // Long-press was triggered — flyout is open, don't fire ConnectCommand
+            e.Handled = true;
+        }
+    }
+
+    private void OnCloseRequested()
+    {
+        // Hide the flyout
+        _connectionFlyout?.Hide();
+    }
+
     private void ToolLb_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         e.Handled = true;
         SplitB.Flyout?.Hide();
     }
+
     private async Task<IReadOnlyList<IStorageFile>?> InteractionHandler(string input)
     {
         var topLevel = TopLevel.GetTopLevel(this);
@@ -55,11 +104,4 @@ public partial class MainView : UserControl
             });
         return storageFiles;
     }
-
-    //private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
-    //{
-    //    e.Handled = true;
-    //    SplitB.Flyout?.Hide();
-    //    Debug.WriteLine("flyout");
-    //}
 }
