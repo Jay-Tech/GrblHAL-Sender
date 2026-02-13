ebbcf2f5a5407c8fc5d45f905c7a7840651d4db0
Jay-Tech <Jayh160@yahoo.com>
2026-02-13 16:12:01 -0600

    rework for console gcode panel and flyout

---
+++ GrblHAL_Sender/GrbLHALSender/Views/ConsoleOutputView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GrbLHALSender.Views;
+
+public partial class ConsoleOutputView : UserControl
+{
+    public ConsoleOutputView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ GrblHAL_Sender/GrbLHALSender/Views/GcodeOutputView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GrbLHALSender.Views;
+
+public partial class GcodeOutputView : UserControl
+{
+    public GcodeOutputView()
+    {
+        InitializeComponent();
+    }
+}
