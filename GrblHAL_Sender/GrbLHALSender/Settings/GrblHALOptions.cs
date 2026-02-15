aba1c6cb1b6084525360fd74190f62a012deed64
Jay-Tech <Jayh160@yahoo.com>
2026-02-15 02:26:05 -0600

    fixed shutter in job running reworked,  prefill  buffer based on grblhal reported  block and buffer  size

--- GrblHAL_Sender/GrbLHALSender/Settings/GrblHALOptions.cs
+++ GrblHAL_Sender/GrbLHALSender/Settings/GrblHALOptions.cs
@@ -6,6 +6,8 @@ public class GrblHALOptions
 {
     public int AxesCount { get; set; }
     public int ToolTableCount { get; set; }
+    public int BlockBufferSize { get; set; }
+    public int RxBufferSize { get; set; }
     public bool HasToolTable => ToolTableCount > 0;
     public List<char> AxisLabels { get; set; } = new();
     public List<char> SignalLabels { get; set; } = new();
